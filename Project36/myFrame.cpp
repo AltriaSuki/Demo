3533732b3bedcc3179eeb22105687ceb00ab4dd7
AltriaSuki <1341951274@qq.com>
2024-10-17 15:24:41 +0800

    new

--- Project36/myFrame.cpp
+++ Project36/myFrame.cpp
@@ -2,33 +2,38 @@
 #include"draw.h"
 #include"MyCanvas.h"
 #include <wx/splitter.h>
-MyFrame::MyFrame(const wxString& title) :wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(600, 400)) {
-	//menuBar = new wxMenuBar;
-	//file = new wxMenu;
-	//file->Append(wxID_ANY, wxT("&News"));
-	//file->Append(wxID_ANY, wxT("&Open"));
-	//file->Append(wxID_ANY, wxT("&Save"));
-	//file->AppendSeparator();
-	//imp = new wxMenu;
-	//imp->Append(wxID_ANY, wxT("import from 1"));
-	//imp->Append(wxID_ANY, wxT("import from 2"));
-	//file->AppendSubMenu(imp, wxT("import"));
-	//file->Append(wxID_CLOSE, wxT("&Quit"));
-	//file->Append(wxID_ABOUT, "About", "Show something about");
-	//menuBar->Append(file, wxT("&File"));
-	//SetMenuBar(menuBar);
-	//CreateStatusBar(2);//×´Ì¬À¸
-	//SetStatusText(wxT("welcome to wxWidgets!"));
-	////Connect(wxID_CLOSE, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(SimpleMenu::onQuit));
-	//Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::onQuit, this, wxID_CLOSE);
-	//Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::onAbout, this, wxID_ABOUT);
-	//Centre();
-	//wxSplitterWindow* splitter = new wxSplitterWindow(this);
-	//wxPanel* controlPanel = new wxPanel(splitter);
-	//Draw* drawPanel = new Draw(splitter);
-	//splitter->SplitVertically(controlPanel, drawPanel, 100);
-	//Bind(wxEVT_SIZE, &MyFrame::OnResize, this);
-	MyCanvas* canvas = new MyCanvas(this);
+MyFrame::MyFrame(const wxString& title) :wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(1200, 800)) {
+	menuBar = new wxMenuBar;
+	file = new wxMenu;
+	file->Append(wxID_ANY, wxT("&News"));
+	file->Append(wxID_ANY, wxT("&Open"));
+	file->Append(wxID_ANY, wxT("&Save"));
+	file->AppendSeparator();
+	imp = new wxMenu;
+	imp->Append(wxID_ANY, wxT("import from 1"));
+	imp->Append(wxID_ANY, wxT("import from 2"));
+	file->AppendSubMenu(imp, wxT("import"));
+	file->Append(wxID_CLOSE, wxT("&Quit"));
+	file->Append(wxID_ABOUT, "About", "Show something about");
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+	
+	menuBar->Append(file, wxT("&File"));
+	SetMenuBar(menuBar);
+	CreateStatusBar(2);//×´Ì¬À¸
+	SetStatusText(wxT("welcome to wxWidgets!"));
+	//Connect(wxID_CLOSE, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(SimpleMenu::onQuit));
+	Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::onQuit, this, wxID_CLOSE);
+	Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::onAbout, this, wxID_ABOUT);
+	Centre();
+	wxSplitterWindow* splitter = new wxSplitterWindow(this);
+	wxPanel* controlPanel = new wxPanel(splitter);
+	Draw* drawPanel = new Draw(splitter);
+	splitter->SplitVertically(controlPanel, drawPanel, 100);
+	/*sizer->Add(controlPanel, 1, wxEXPAND | wxALL, 10);
+	sizer->Add(drawPanel, 1, wxEXPAND | wxALL, 10);
+	SetSizer(sizer);
+	GetAutoLayout();*/
+	Bind(wxEVT_SIZE, &MyFrame::OnResize, this);
 
 }
 void MyFrame::OnResize(wxSizeEvent& event) {
